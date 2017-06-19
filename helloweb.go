ea0acf84562a1b012a63b04b8548af969098b5ed
hephaestic <hephaestic@gmail.com>
2017-06-19 02:31:01 -0700

    added Proc, .godir. dynamicised port

--- helloweb.go
+++ helloweb.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "Hello, Web")
 	})
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":" + port, nil)
 }
 
 
